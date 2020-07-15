70174723f2b6fdd5f3933e4531f999b2cdc0708c
Hans Martin <hansmrtn@gmail.com>
2020-07-15 12:58:38 -0700

    update reactive logic

--- R/mod_main_panel.R
+++ R/mod_main_panel.R
@@ -11,9 +11,7 @@
 #' @importFrom shinyWidgets pickerInput airDatepickerInput radioGroupButtons sliderTextInput
 mod_main_panel_ui <- function(id){
   ns <- NS(id)
-  
   TZ <- 'UTC'
-  
   tagList(
     pickerInput(
       inputId = ns("community_picker"),
@@ -25,7 +23,6 @@ mod_main_panel_ui <- function(id){
         title = "Select community...",
         size = 7 )
     ),
-    
     pickerInput(
       inputId = ns("sensor_picker"),
       label = tags$h4("Sensor"),
@@ -36,7 +33,6 @@ mod_main_panel_ui <- function(id){
         title = "Select sensor...",
         size = 7 )
     ),
-    
     airDatepickerInput(
       inputId = ns("date_picker"),
       label = tags$h4("Date"),
@@ -56,9 +52,7 @@ mod_main_panel_ui <- function(id){
       choices = seq(from = 2, to = 30, by = 1), 
       grid = TRUE, 
       selected = 5
-      
     )
-    
     # radioGroupButtons(
     #   inputId = ns("lookback_picker"),
     #   label = tags$h4("View Past"),
@@ -73,7 +67,6 @@ mod_main_panel_ui <- function(id){
     #     yes = tags$i(class = "fa fa-check",
     #                  style = "color: #008cba"))
     # )
-    
   )
 }
 
@@ -95,85 +88,16 @@ mod_main_panel_server <- function(input, output, session, values){
   # SCAQMD sensors
   setArchiveBaseUrl("http://data.mazamascience.com/PurpleAir/v1") 
   
-  ## sensors future
-  sensors <- reactive({
-    future({ sensor_load() })
-  })
-  
-  # Add the promised sensor obj to reactive values when loaded
-  observeEvent(sensors(), {
-    logger.trace("loading sensor obj...")
-    then(sensors(), function(d) { values$sensors <- d })
-    catch(sensors(), function(e) { logger.error(e) })
-    logger.trace("done.")
-  })
-  
-  ## pas future
-  pas <- reactive({
-    future({ pas_load() })
-  })
-  
-  # Add the promised pas obj to reactive values when loaded
-  observeEvent(pas(), {
-    logger.trace("loading pas obj...")
-    then(pas(), function(d) { values$pas <- d } )
-    catch(pas(), function(e) { logger.error(e) })
-    logger.trace("done.")
-  })
-  
-  ## pat obj future
-  pat <- reactive({
-    # Require a sensor selection
-    req(input$sensor_picker) 
-    # Require pas 
-    req(values$pas)
-    ed <- ymd(input$date_picker) + days(1)
-    sd <- ed - days(input$lookback_picker) #days(31) #years(1) # Default to load 31 days
-    future({ 
-      pat_load(
-        pas = values$pas, 
-        label = input$sensor_picker, 
-        startdate = sd, 
-        enddate = ed
-      ) 
+  ## ---- Startup ----
+  # load non-changing rv: sensors, pas objs
+  observe({
+    # create sensors obj promise
+    sensors <- future({ 
+      logger.trace("loading sensors obj...")
+      sensor_load() 
     })
-  })
-  
-  # Add the promised pat obj to reactive values when loaded
-  observeEvent(pat(), {
-    logger.trace(paste("loading", input$sensor_picker, "pat obj...")) 
-    then(pat(), function(d) { values$pat <- d })
-    catch(pat(), function(e) { logger.error(e) })
-    logger.trace(paste(input$sensor_picker, "done."))
-  })
-  
-  ## latest pat obj future
-  latest <- reactive({
-    req(input$sensor_picker)
-    req(values$pas)
-    future({
-      pat_createNew(
-        pas = values$pas, 
-        label = input$sensor_picker, 
-        timezone = 'UTC'
-      ) 
-    })
-  })
-  
-  # Add the promised latest pat obj to reactive values when on latest navbar tab
-  observeEvent({ values$nav; input$sensor_picker }, {
-    if ( values$nav == 'latest' ) {
-      req(input$sensor_picker)
-      logger.trace(paste("loading latest", input$sensor_picker, "pat obj..."))
-      then(latest(), function(d) { values$latest <- d })
-      catch(latest(), function(e) { logger.error(e) })
-      logger.trace(paste(input$sensor_picker, "latest done."))
-    }
-  })
-  
-  # Update the pickers when sensor promise is fulfilled
-  observeEvent(sensors(), {
-    then(sensors(), function(d) {
+    then(sensors, function(d) {
+      # update the pickers fulfilled sensor promise
       updatePickerInput(
         session, 
         "community_picker", 
@@ -184,46 +108,81 @@ mod_main_panel_server <- function(input, output, session, values){
         "sensor_picker", 
         choices = d$meta$label
       )
+      # update sensors rv
+      values$sensors <- d 
+      logger.trace("sensors done.")
+    })
+    catch(sensors, function(err) { 
+      logger.error(err) 
+    })
+    # create pas promise
+    pas <- future({ 
+      logger.trace("loading pas obj...")
+      pas_load() 
+    })
+    then(pas, function(d) { 
+      # update pas rv 
+      values$pas <- d 
+      logger.trace("pas done.")
+    })
+    catch(pas, function(err) { 
+      logger.error(err) 
     })
   })
   
-  # Update the available sensors in selected community
-  observeEvent(input$community_picker, {
-    req(input$community_picker)
-    then(sensors(), function(d) {
-      logger.trace(paste("selected community: ", input$community_picker))
-      if ( input$community_picker == "All..." ) {
-        community_sensors <- d$meta
-      } else {
-        community_sensors <- 
-          d$meta[id2com(d$meta$communityRegion) == input$community_picker,]
-      }
-      updatePickerInput(
-        session, 
-        "sensor_picker", 
-        choices = community_sensors$label
-      )
+  ## ---- Reactive Expressions ----
+  # load pat reactive expression 
+  getPat <- reactive({
+    req(input$sensor_picker, values$pas)
+    ed <- ymd(input$date_picker) + days(1)
+    sd <- ed - days(input$lookback_picker) #days(31) #years(1) 
+    pat <- future({ 
+      logger.trace(paste("loading", input$sensor_picker, "pat obj..."))
+      pat_load(
+        pas = values$pas, 
+        label = input$sensor_picker, 
+        startdate = sd, 
+        enddate = ed
+      ) 
     })
+    then(pat, function(d) {
+      values$pat <- d 
+      logger.trace(paste(input$sensor_picker, "pat done."))
+    })
+    catch(pat, function(err) {
+      logger.error(err)
+    })
+    # return pat promise for flexibility
+    return(pat)
   })
-  
-  # Update the reactive sensor on input selection
-  observeEvent(input$sensor_picker, {
-    req(input$sensor_picker)
-    logger.trace(paste("selected sensor: ", input$sensor_picker))
-    # Update the selected sensor reactive data
-    values$sensor <- 
+  # filter sensors reactive expression
+  getSensor <- reactive({
+    req(input$sensor_picker, values$sensors)
+    # update the selected sensor reactive data
+    sensor <- future({ 
       sensor_filterMeta(values$sensors, .data$label == input$sensor_picker)
+    })
+    then(sensor, function(d) {
+      values$sensor <- d 
+      logger.trace(paste(input$sensor_picker, "sensor done."))
+    })
+    catch(sensor, function(err) {
+      logger.error(err)
+    })
+    # return sensor promise for flexibility
+    return(sensor)
   })
-  
-  # Update the reactive pat values on date range changes
-  observeEvent({ input$date_picker; input$lookback_picker } , {
-    req(values$pat)
+  # filter pat obj dates reactive expression
+  filterDates <- reactive({
+    req(values$pat, input$date_picker, input$lookback_picker)
+    # get data time domain 
     data_sd <- ymd_hms(min(values$pat$data$datetime))
     data_ed <- ymd_hms(max(values$pat$data$datetime))
-    
+    # get selection time domain
     ed <- ymd(input$date_picker)
     sd <- ed - days(input$lookback_picker)
-    
+    # if selection date domain within data date domain, filter pat obj
+    # NOTE: this chunk -updates- the pat rv obj
     if ( sd %within% (data_sd %--% data_ed) ) {
       logger.trace(paste(input$sensor_picker, "filter date to", sd, "--", ed))
       values$pat <- pat_filterDate(
@@ -231,17 +190,70 @@ mod_main_panel_server <- function(input, output, session, values){
         startdate = strftime(sd, "%Y%m%d"),
         enddate = strftime(ed, "%Y%m%d")
       )
+    # otherwise reload pat obj with domain selections
     } else {
       logger.trace(paste(input$sensor_picker, "selected dates", sd, "--", 
                          ed, "out of range, reloading pat obj..."))
-      then(pat(), function(d) {
-        values$pat <- d
-        logger.trace(paste(input$sensor_picker, "done."))
-      })
+      getPat()
+      logger.trace(paste(input$sensor_picker, "done."))
     }
+    # return start/end dates for flex
+    return(c(sd, ed))
   })
+  # load latest reactive expression
+  getLatest <- reactive({
+    req(input$sensor_picker, values$pas)
+    latest <- future({ 
+      logger.trace(paste("loading latest", input$sensor_picker, "pat obj..."))
+      pat_createNew(
+        pas = values$pas, 
+        label = input$sensor_picker, 
+        timezone = 'UTC'
+      ) 
+    })
+    then(latest, function(d) {
+      values$latest <- d
+      logger.trace(paste("latest", input$sensor_picker, "pat done."))
+    })
+    catch(latest, function(err) {
+      logger.trace(err)
+    })
+    return(latest)
+  })
+
+  ## ---- Event Handling ----
+  # load the pat and filter the sensors on sensor selection
+  observeEvent(input$sensor_picker, {
+    # load the pat obj and filtered sensor from selection label
+    getPat()
+    getSensor()
+  }, priority = 0)
+  # filter dates on date || lookback picker change
+  observeEvent({ input$date_picker; input$lookback_picker }, {
+    filterDates()
+  }, priority = 0)
+  # filter sensors selection on community selection
+  observeEvent(input$community_picker, {
+    req(input$community_picker, values$sensors)
+    logger.trace(paste("selected community: ", input$community_picker))
+    if ( input$community_picker == "All..." ) {
+      validSensors <- values$sensors$meta
+    } else {
+      validSensors <- 
+        values$sensors$meta[
+          id2com(values$sensors$meta$communityRegion) == input$community_picker,
+        ]
+    }
+    updatePickerInput(session, "sensor_picker", choices = validSensors$label)
+  }, priority = 0)
+  # load latest pat obj on latest nav && sensor selection
+  observeEvent({ input$sensor_picker; values$navbar }, {
+    if ( values$navbar == "latest" ) {
+      getLatest()
+    }
+  }, priority = 1) # if on latest page, load latest pat first
   
-}
+} # End Server
 
 ## To be copied in the UI
 # mod_main_panel_ui("main_panel_ui_1")
