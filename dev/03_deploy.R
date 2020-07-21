da2557d7589067747f4cf14e65b0d76df9c4cf4f
Hans Martin <hansmrtn@gmail.com>
2020-07-21 10:44:31 -0700

    added generate dockerfile

--- dev/03_deploy.R
+++ dev/03_deploy.R
@@ -20,18 +20,11 @@ rhub::check_for_cran()
 
 # Deploy
 
-## RStudio ----
-## If you want to deploy on RStudio related platforms
-golem::add_rstudioconnect_file()
-golem::add_shinyappsio_file()
-golem::add_shinyserver_file()
-
 ## Docker ----
-## If you want to deploy via a generic Dockerfile
-golem::add_dockerfile()
-
-## If you want to deploy to ShinyProxy
-golem::add_dockerfile_shinyproxy()
-
-## If you want to deploy to Heroku
-golem::add_dockerfile_heroku()
+golem::add_dockerfile(
+  output = "docker/Dockerfile", 
+  port = 3838, 
+  sysreqs = TRUE, 
+  expand = TRUE,
+  repos = 'https://cran.rstudio.com/'
+)
