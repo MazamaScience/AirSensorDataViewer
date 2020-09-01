796ac10067755efa2788f036149edc82ffe0eda6
Hans Martin <hansmrtn@gmail.com>
2020-09-01 13:11:28 -0700

    reimplemented noaa data. removed worldmet dep

--- R/fct_load.R
+++ R/fct_load.R
@@ -188,24 +188,58 @@ get_pat_latest <- function(pas, label, tz = 'UTC') {
 #' @examples
 #' 
 #' @importFrom worldmet importNOAA getMeta 
-#' @importFrom lubridate ymd `%--%` `%within%`
-get_noaa <- function(sensor) {
+#' @importFrom lubridate ymd `%--%` `%within%` year ymd_hms round_date
+#' @importFrom data.table fread data.table tstrsplit `%between%`
+#' @importFrom geodist geodist
+#' @importFrom xts period.apply endpoints
+get_noaa <- function(sensor, sd, ed) {
   # logger.trace("loading NOAA...")
   tryCatch(
     expr = {
-      sd <- min(ymd_hms(sensor$data$datetime))
-      ed <- max(ymd_hms(sensor$data$datetime))
+      
       # Find wind data readings from the closest NOAA site
-      year <- year(ed)
+      yr <- year(ed)
       lon <- sensor$meta$longitude
       lat <- sensor$meta$latitude
-      sd <- min(ymd(sensor$data$datetime))
-      ed <- max(ymd(sensor$data$datetime))
-      closestSite <- getMeta(lon = lon, lat = lat,n = 1, plot = FALSE)
-      siteCode <- closestSite$code
-      siteData <- importNOAA(code = siteCode, year = year, n.cores = 1) %>% 
-        filter(.data$date %within% (ymd(sd) %--% ymd(ed)))
-      return(siteData)
+      
+      metaUrl <- "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.csv"
+      meta <- fread(metaUrl)[ STATE == "CA"
+      ][ (ymd(sd) %--% ymd(ed)) %within% (ymd(BEGIN) %--% ymd(END))
+      ][, dist := geodist(cbind("Longitude" = LON, "Latitude" = LAT), cbind("Longitude" = lon, "Latitude" = lat), pad = TRUE) 
+      ][ order(dist)
+      ][, code := paste0(USAF, WBAN) ][1,]
+      
+      dataUrl <- paste0("https://www.ncei.noaa.gov/data/global-hourly/access/", yr, "/", meta$code, ".csv")
+      # Install package bit64
+      data <- fread(dataUrl)[, c("wd", "x", "y", "ws", "z") := tstrsplit(WND, ",")
+      ][, wd := ifelse(as.numeric(wd) == 999, NA, as.numeric(wd))
+      ][, ws := ifelse(as.numeric(ws) == 9999, NA, as.numeric(ws))/10 
+      ][, c("air_temp", "flag_temp") := tstrsplit(TMP, ",")
+      ][, air_temp := ifelse(as.numeric(air_temp) == 9999, NA, as.numeric(air_temp)/10)
+      ][, c("dew_point", "flag_dew") := tstrsplit(DEW, ",")
+      ][, dew_point := ifelse(as.numeric(dew_point) == 9999, NA, as.numeric(dew_point)/10)
+      ][, date := ymd_hms(DATE) 
+      ][, RH :=  100 * ((112 - 0.1 * air_temp + dew_point) / (112 + 0.9 * air_temp))^8 
+      ][, c("date", "wd",  "ws", "air_temp", "RH") ]
+      setcolorder(data, "date")
+      
+      dxts <- as.xts.data.table(data)
+      ep <- endpoints(dxts, "hour")
+      
+      hourly <- as.data.table(do.call(
+        cbind, 
+        lapply(
+          dxts, 
+          function(x) { 
+            period.apply(x, ep, function(x) { mean(x, na.rm=TRUE) }) 
+          }
+        )
+      ))
+      
+      hourly[, date := round_date(index, "hour")
+      ][, index := NULL 
+      ][ date %between% c(sd, ed) ]
+      
     }, 
     error = function(err) {
       catchError(err)
