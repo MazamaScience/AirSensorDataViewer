2d393d83e410a58030b23b429ad8b439d26f3993
Hans Martin <hansmrtn@gmail.com>
2020-09-09 09:23:02 -0700

    re-documented user class

--- R/User.R
+++ R/User.R
@@ -1,39 +1,75 @@
-#'@export
-#'@importFrom future future value
-#'@importFrom AirSensor setArchiveBaseUrl
-#'@import MazamaCoreUtils
+#' User R6 Class 
+#' 
+#' @description 
+#' Create a user to act as a data and state storage object for AirSensor 
+#' DataViewer Shiny Application.
+#' 
+#' @details 
+#' Create an object that is responsible for housing the core state and data components of a 
+#' Shiny applications user session. Each user is dependent on a Shiny session object. 
+#' The methods and data of the object can be shared across modules without the requirment 
+#' to create reactive-values. 
+#'  
+#' @export
+#' @importFrom future future value
+#' @importFrom AirSensor setArchiveBaseUrl
+#' @import MazamaCoreUtils
 User <- R6::R6Class(
   "User", 
   
   active = list(
+    
+    #' @details 
+    #' \code{User$pas}: Return a future pa-synoptic object. 
     pas = function() {
       private$rx_pas$depend()
       return(private$pas_promise)
     },
+    
+    #' @details 
+    #' \code{User$sensors}: Return a future airsensor object. 
     sensors = function() {
       private$rx_sensors$depend()
       return(private$sensors_promise)
     },
+    
+    #' @details 
+    #' \code{User$pat}: Return a future pa_timeseries object. 
     pat = function() {
       private$rx_pat$depend()
       return(private$pat_promise)
     }, 
+    
+    #' @details 
+    #' \code{User$sensor}: Return a future filtered airsensor object.
     sensor = function() {
       private$rx_sensor$depend() 
       return(private$sensor_promise)
     }, 
+    
+    #' @details 
+    #' \code{User$pwfsl}: Return a future ws_monitor object from the PWFSLSmoke Package. 
     pwfsl = function() {
       private$rx_pwfsl$depend()
       return(private$pwfsl_promise)
     }, 
+    
+    #' @details 
+    #' \code{User$latest}: Return a future latest pa-timeseries object (from \code{pat_createNew()}). 
     latest = function() {
       private$rx_latest$depend() 
       return(private$latest_promise)
     }, 
+    
+    #' @details 
+    #' \code{User$annual}: Return an annual future airsensor object.
     annual = function() {
       private$rx_annual$depend()
       return(private$annual_promise)
     },
+    
+    #' @details 
+    #' \code{User$noaa}: Return a future dataframe generated from NOAA Database. 
     noaa = function() {
       private$rx_noaa$depend()
       return(private$noaa_promise)
@@ -43,11 +79,21 @@ User <- R6::R6Class(
   public = list(
     
     baseUrl = "http://data.mazamascience.com/PurpleAir/v1", 
+    
+    #' @details
+    #' \code{User$selected$...}: Access Shiny Input reactive values.
     selected = NULL,
+    
+    #' @details 
+    #' \code{User$url}: Access the current page URL for bookmarking state.
     url = NULL, 
+    
     tz = NULL, 
     token = NULL,
     
+    #' @details 
+    #' Create a user. 
+    #' @param session A Shiny session object.
     initialize = function(session) {
       
       logger.trace(paste("User started on session token:", session$token)) 
@@ -84,7 +130,9 @@ User <- R6::R6Class(
       })
     }, 
     
-    # -- Update the PAS promise
+    #' @details 
+    #' Update the users pa_synoptic data. 
+    #' @param date YYYYmmdd.
     updatePas = function(date = NULL) {
       logger.trace(paste("Updating pas ===>"))
       private$rx_pas$trigger()
@@ -99,7 +147,10 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     }, 
     
-    # -- Update the sensors promise
+    #' @details 
+    #' Update the users airsensor object.
+    #' @param sd startdate YYYYmmdd.
+    #' @param ed enddate YYYYmmdd.
     updateSensors = function(sd, ed) {
       logger.trace(paste("Updating sensors ===>", sd, ed))
       private$rx_sensors$trigger()
@@ -109,7 +160,10 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     },
     
-    # -- Update the PAT promise
+    #' @details 
+    #' Update the users pa_timeseries object.
+    #' @param sd startdate YYYYmmdd.
+    #' @param ed enddate YYYYmmdd.
     updatePat = function(label, sd, ed) {
       logger.trace(paste("Updating pat ===>", label, sd, ed))
       private$rx_pat$trigger()
@@ -125,7 +179,10 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     }, 
     
-    # -- Update the sensor promise
+    #' @details 
+    #' Update the users filtered airsensor object. 
+    #' Commonly used when a user selects a different sensor. 
+    #' @param label the sensor label to filter the airsensor object by.
     updateSensor = function(label) {
       logger.trace(paste("updating sensor ===>", label))
       private$rx_sensor$trigger()
@@ -138,7 +195,11 @@ User <- R6::R6Class(
       
     },
     
-    # -- Update the pwfsl monitor promise
+    #' @details 
+    #' Update the users ws_monitor object.
+    #' @param label ?
+    #' @param sd startdate YYYYmmdd.
+    #' @param ed enddate YYYYmmdd.
     updatePwfsl = function(label, sd, ed) {
       logger.trace(paste("Updating pwfsl ===>", sd, ed))
       private$rx_pwfsl$trigger()
@@ -149,7 +210,10 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     }, 
     
-    # -- Update the latest pat promise
+    #' @details 
+    #' Update the users latest pa_timeseries object.
+    #' @param label a label of the sensor.
+    #' @param tz a specified timezone. (default: UTC)
     updateLatest = function(label, tz = 'UTC') {
       logger.trace(paste("Updating latest ===>", label))
       private$rx_latest$trigger()
@@ -159,7 +223,9 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     }, 
     
-    # -- Update the annual pat 
+    #' @details 
+    #' Update the users annual airsensor object.
+    #' @param date a date to parse year from.
     updateAnnual = function(date) {
       logger.trace(paste("Updating annual ===>"), date)
       private$rx_annual$trigger()
@@ -170,7 +236,10 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     },
     
-    # -- Update the noaa data
+    #' @details 
+    #' Update the users NOAA data.
+    #' @param sd startdate YYYYmmdd.
+    #' @param ed enddate YYYYmmdd.
     updateNoaa =  function(sd, ed) {
       logger.trace(paste("Updating noaa ===>"))
       private$rx_noaa$trigger()
@@ -180,6 +249,9 @@ User <- R6::R6Class(
       }, lazy = TRUE)
     },
     
+    #' @details 
+    #' Set the users Timezone.
+    #' @param timezone a valid timezone string.
     setTz = function(timezone) {
       logger.trace(paste("setting timezone ===>", timezone))
       self$tz <- timezone
