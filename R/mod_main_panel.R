d5ebae6f2bb41a7049fd39950fceed847dd1fce6
Hans Martin <hansmrtn@gmail.com>
2020-09-25 15:58:08 -0700

    Reworked some icons

--- R/mod_main_panel.R
+++ R/mod_main_panel.R
@@ -141,9 +141,9 @@ mod_main_panel_ui <- function(id) {
               .transition()
               .duration(1000)
               .attr('visibility', 'visible');
-          
+            
           };
-          ", 
+        ", 
         functions = c("communityFilter")
       ), 
       
@@ -396,18 +396,22 @@ mod_main_panel_server <- function(input, output, session, usr) {
   )
   
   output$date_range_label <- renderUI({
-    
-    # sd <- lubridate::ymd(input$date_select) - lubridate::days(input$past_select)
-    # ed <- lubridate::ymd(input$date_select)
-    # 
-    # 
-    # usr$selected$sd <- sd 
-    # usr$selected$ed <- ed
    
-   tags$h5(tags$div(HTML(paste0('<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-event" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    tags$h5(
+      tags$div(
+        HTML(
+          paste0(
+            '<svg width="1.25em" height="0.8em" viewBox="0 0 16 16" class="bi bi-calendar-week" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
-</svg> ', strftime(usr$selected$sd, "%B %d, %Y"), "  -  ", strftime(usr$selected$ed, "%B %d, %Y")))))
+  <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+</svg>', 
+            strftime(usr$selected$sd, "%B %d, %Y"), 
+            "  -  ", 
+            strftime(usr$selected$ed, "%B %d, %Y")
+          )
+        )
+      )
+    )
    
    
   })
